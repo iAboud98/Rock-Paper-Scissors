bad349544d965aad3ff8e0b25c078322e9db3dc2
iAboud98 <aboudfialah98@gmail.com>
2024-03-01 21:36:52 +0200

    add ASCII art

--- Rock-Paper-Scissors.c
+++ Rock-Paper-Scissors.c
@@ -15,19 +15,19 @@ int main (){
     int computerChoice = min + rand() % (max - min + 1);
     
     if(choice == 1){
-        printf("\nYou choose Rock");
+        printf("\nYou choose Rock\n    _______\n---'   ____)\n      (_____)\n      (_____)\n      (____)\n---.__(___)\n");
     }else if (choice == 2){
-        printf("\nYou choose Paper");
+        printf("\nYou choose Paper\n     _______\n---'    ____)____\n           ______)\n          _______)\n         _______)\n---.__________)\n");
     }else if(choice == 3){
-        printf("\nYou choose Scissors");
+        printf("\nYou choose Scissors\n    _______\n---'   ____)____\n          ______)\n       __________)\n      (____)\n---.__(___)\n");
     }
 
     if(computerChoice == 1){
-        printf("\nComputer choose Rock");
+        printf("\nComputer choose Rock\n    _______\n---'   ____)\n      (_____)\n      (_____)\n      (____)\n---.__(___)\n");
     }else if (computerChoice == 2 ){
-        printf("\nComputer choose Paper");
+        printf("\nComputer choose Paper\n     _______\n---'    ____)____\n           ______)\n          _______)\n         _______)\n---.__________)\n");
     }else if (computerChoice == 3){
-        printf("\nComputer choose Scissors");
+        printf("\nComputer choose Scissors\n    _______\n---'   ____)____\n          ______)\n       __________)\n      (____)\n---.__(___)\n");
     }
 
     if (choice == computerChoice){
