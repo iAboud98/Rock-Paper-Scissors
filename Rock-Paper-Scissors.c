d9718dbe6bbc122bcf341dc95f2343619612a754
iAboud98 <aboudfialah98@gmail.com>
2024-03-01 21:09:39 +0200

    set up user choice

--- Rock-Paper-Scissors.c
+++ Rock-Paper-Scissors.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main (){
-    printf("Hey");
+
+    int choice;
+    printf("Choose 1 for Rock, 2 for Paper, 3 for Scissors :   ");
+    scanf("%d",&choice);
+    
+    if(choice == 1){
+        printf("\nYou choose Rock");
+    }else if (choice == 2){
+        printf("\nYou choose Paper");
+    }else if(choice == 3){
+        printf("\nYou choose Scissors");
+    }
+
+
     return 0;
 }
